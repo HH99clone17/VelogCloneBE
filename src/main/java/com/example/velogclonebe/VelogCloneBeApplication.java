3ef1e97e6b8afc903825b44ea451df3c23408b0b
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-07-16 17:21:39 +0900

    set_project

---
+++ src/main/java/com/example/velogclonebe/VelogCloneBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.velogclonebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelogCloneBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelogCloneBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/velogclonebe/VelogCloneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.velogclonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelogCloneBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
