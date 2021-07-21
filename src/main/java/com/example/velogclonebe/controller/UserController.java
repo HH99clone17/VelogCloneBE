d32086f3f70e4517348766a8ef6b0a703d57249d
SOULIKK <42605239+SOULIKK@users.noreply.github.com>
2021-07-21 13:38:52 +0900

    유저 정보 호출

--- src/main/java/com/example/velogclonebe/controller/UserController.java
+++ src/main/java/com/example/velogclonebe/controller/UserController.java
@@ -1,14 +1,21 @@
 package com.example.velogclonebe.controller;
 
 
+import com.example.velogclonebe.domain.dto.response.ArticleListResponseDto;
+import com.example.velogclonebe.domain.dto.response.UserInfoResponseDto;
 import com.example.velogclonebe.domain.entity.User;
 import com.example.velogclonebe.domain.repository.UserRepository;
+import com.example.velogclonebe.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,12 +24,20 @@ public class UserController {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    private final UserService userService;
+
     @PostMapping("join")
     public String join(@RequestBody User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         user.setRoles("ROLE_USER");
         userRepository.save(user);
         return "회원가입 완료";
+    }
+
+    @GetMapping("/api/user")
+    public UserInfoResponseDto getUserInfo(@AuthenticationPrincipal UserDetails userDetails) {
+        String username = userDetails.getUsername();
+        return userService.getUserInfo(username);
     }
 
     // TEST - user, manager, admin 권한 접근 가능
---
+++ src/main/java/com/example/velogclonebe/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.velogclonebe.service;
+
+
+import com.example.velogclonebe.domain.dto.response.UserInfoResponseDto;
+import com.example.velogclonebe.domain.entity.User;
+import com.example.velogclonebe.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+    // 토큰으로 유저정보 가져오기
+    @Transactional
+    public UserInfoResponseDto getUserInfo(String username) {
+
+        User user = userRepository.findByUsername(username);
+        UserInfoResponseDto userInfoResponseDto = new UserInfoResponseDto(user);
+        return userInfoResponseDto;
+    }
+}
